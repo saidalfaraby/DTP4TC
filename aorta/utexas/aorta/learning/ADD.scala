b27abb24d4b10d68da46c74a5addba7560677478
saidalfaraby <said.al.faraby@gmail.com>
2014-03-02 09:42:17 +0100

    Update ADD

--- aorta/utexas/aorta/learning/ADD.scala
+++ aorta/utexas/aorta/learning/ADD.scala
@@ -2,10 +2,22 @@ package learning
 
 import scala.collection.mutable.LinkedHashMap
 
+/**
+ * Decision node is a node whose probability of its values are going to be predicted
+ * @param internalNode - in out case these are array of parents labels
+ * @param decisionNodeName - label of decision node
+ * @param decisionNodeVal - all possible values/states of decision node
+ */
 class ADD (internalNode : Array[String], decisionNodeName : String, decisionNodeVal : Array[String]){
-
-  val root = new Node(internalNode(0))
-
+  //Assume the order of internalNode is fix, and the first one is the root
+  val root = new Node(internalNode(0)) 
+  
+  def getName = decisionNodeName
+  
+  /**
+   * @param state - values/states of all internal nodes at time step n
+   * @param value - value/state of decision node at time step n+1
+   */
   def update(state : Array[String], value : String){
     require(state.length == internalNode.length)
     def recur(parent : Node, edgeNames : Array[String]){
@@ -17,7 +29,7 @@ class ADD (internalNode : Array[String], decisionNodeName : String, decisionNode
         try { //get leaf
           val leaf = parent.getChild(edgeLabel).asInstanceOf[Leaf]
           leaf.hit(value)
-        } catch {
+        } catch { //leaf doesn't exist, create the new one
           case e:Exception => {
             val leaf = new Leaf(decisionNodeName, decisionNodeVal)//create new leaf
             leaf.hit(value)
@@ -26,9 +38,9 @@ class ADD (internalNode : Array[String], decisionNodeName : String, decisionNode
         }
       } else { //internal node
       var childNode : Node = null
-        try {
+        try { // get internal node
           childNode = parent.getChild(edgeLabel).asInstanceOf[Node]
-        } catch {
+        } catch { //internal node doesn't exist, create the new one
           case e:Exception => {
             val nodeName = internalNode(internalNode.length-edgeNames.length+1)
             childNode = new Node(nodeName)
@@ -51,6 +63,7 @@ class ADD (internalNode : Array[String], decisionNodeName : String, decisionNode
         remaining=queue.length
       }
       var curNode = queue(0)
+      //remove proceeded node
       queue = queue.slice(1,queue.length)
       print (curNode.toString+"  ")
       //expand current Node
@@ -59,6 +72,58 @@ class ADD (internalNode : Array[String], decisionNodeName : String, decisionNode
       }
       remaining-=1
     }
+
+  }
+  
+  def printToData {
+    //traverse depth first
+    print(getName)
+    def traverseHelper(node : GenericNode){
+      print("\t(")
+      if (node.isInstanceOf[Node]){
+        print(node.toString)
+        val children = node.asInstanceOf[Node].children
+        var count=children.size
+        for (edge <-children.keysIterator){
+          print("\t(")
+          print(edge)
+          traverseHelper(children.getOrElse(edge, null))
+          print(")")
+          if (count>1) 
+            println
+          count -=1
+        }
+        
+      } else if (node.isInstanceOf[Leaf]){
+        print(node.toString)
+      }
+      
+      print(")")
+      
+      
+    }
+    
+    traverseHelper(root)
+    /*
+    var queue : Array[GenericNode] = Array(root)
+    var remaining = 0
+    while (queue.length > 0){
+      if (remaining==0) {
+        println()
+        remaining=queue.length
+      }
+      var curNode = queue(0)
+      //remove proceeded node
+      queue = queue.slice(1,queue.length)
+      print (curNode.toString+"  ")
+      //expand current Node
+      if (!curNode.isInstanceOf[Leaf]){
+        for (nextNode <- curNode.asInstanceOf[Node].children.valuesIterator) queue :+= nextNode
+      }
+      remaining-=1
+    }
+    * 
+    */
 
   }
 }
--- aorta/utexas/aorta/learning/ADDDemo.scala
+++ aorta/utexas/aorta/learning/ADDDemo.scala
@@ -2,14 +2,19 @@ package learning
 
 object ADDDemo {
   def main(args: Array[String]) {
-    var x = new ADD(Array("A","B","C"), "Aprime", Array("True","False","Entah"))
+    var x = new ADD(Array("A","B","C"), "Aprime", Array("True","False"))
+    x.update(Array("True","True","True"),"True")
+    x.update(Array("True","True","False"),"True")
     x.update(Array("True","False","True"),"True")
-    x.update(Array("True","False","True"),"True")
-    x.update(Array("True","False","True"),"Entah")
-    x.update(Array("True","False","True"),"False")
-    x.update(Array("True","False","True"),"Entah")
-    x.printTree
-
+    x.update(Array("True","False","False"),"True")
+    x.update(Array("False","True","True"),"True")
+    x.update(Array("False","True","False"),"True")
+    x.update(Array("False","False","True"),"True")
+    x.update(Array("False","False","False"),"True")
+    x.printToData
+    
+    //var y : String = _
+    //println(y)
 
   }
 
